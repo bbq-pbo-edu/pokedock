abb53c6140a3cb57ee592bb9ef2cd2ae80efc0a9
bbq-po-edu <231660485+bbq-pbo-edu@users.noreply.github.com>
2025-10-24 13:10:25 +0200

    feat: implement POST, GET, PUT, DELETE RestAPI functionality

--- public/index.php
+++ public/index.php
@@ -8,7 +8,45 @@ $entity = $request[0] ?? null;
 $method = $request[1] ?? null;
 $id = $request[2] ?? null;
 
-if (empty($entity)) {
+if ($entity === 'api') {
+    $entity = $request[1] ?? null;
+    $method = $_SERVER['REQUEST_METHOD'] ?? null;
+    $id = $request[2] ?? null;
+
+    switch ($method) {
+        case 'GET':
+            if ($id) {
+                header('Content-type: application/json');
+                echo json_encode(['data' => findById('pokemon', $id)]);
+            }
+            else {
+                header("Content-type: application/json");
+                echo json_encode(['data' => findAll('pokemon')]);
+            }
+            break;
+
+        case 'POST':
+            $data = $_POST;
+            create('pokemon', $data);
+            header('Content-type: application/json');
+            echo json_encode(['data' => findById('pokemon', findLatestId('pokemon'))]);
+            break;
+
+        case 'PUT':
+            $data = file_get_contents('php://input');
+            $data = json_decode($data, true);
+            updateById('pokemon', $id, $data);
+            header('Content-type: application/json');
+            echo json_encode(['data' => findById('pokemon', $id)]);
+            break;
+
+        case 'DELETE':
+            deleteById('pokemon', $id);
+            break;
+    }
+}
+
+else if (empty($entity)) {
     require_once '../view/welcome.php';
 }
 else if ($entity === 'pokemon' && empty($method)) {
@@ -34,6 +72,11 @@ else if ($entity === 'pokemon' && $method === 'edit') {
 }
 else if ($entity === 'pokemon' && $method ==='add') {
     require_once '../src/pokemon/add.php';
+}
+else if ($entity === 'seed') {
+    seedPokemonTable();
+    header('Location: /pokemon');
+    exit();
 }
 else if ($entity === '404') {
     require_once '../view/404.php';
